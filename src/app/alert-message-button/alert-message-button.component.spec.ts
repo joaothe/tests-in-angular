134c10c717370b9f3c95512bda01a5f988098daf
Joao <joaoded@hotmail.com>
2018-07-20 17:24:01 -0300

    Few features

--- src/app/alert-message-button/alert-message-button.component.spec.ts
+++ src/app/alert-message-button/alert-message-button.component.spec.ts
@@ -6,23 +6,35 @@ import { By } from '@angular/platform-browser';
 import { AlertMessageButtonComponent } from './alert-message-button.component';
 
 describe('AlertMessageButtonComponent', () => {
-  let component: AlertMessageButtonComponent;
+  let button_component: AlertMessageButtonComponent;
   let fixture: ComponentFixture<AlertMessageButtonComponent>;
+  let de: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AlertMessageButtonComponent ]
+      declarations: [ AlertMessageButtonComponent ],
+
     })
     .compileComponents();
-  }));
+  }))
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AlertMessageButtonComponent);
-    component = fixture.componentInstance;
+    button_component = fixture.componentInstance;
+    de = fixture.debugElement;
+
     fixture.detectChanges();
-  });
+
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(button_component).toBeTruthy();
   });
-});
+
+  it('should have a message containing `warn`', () => {
+    expect(button_component.content).toContain('warn');
+  })
+
+})
+
+
